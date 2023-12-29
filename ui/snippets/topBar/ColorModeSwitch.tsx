cdb6ea6eb470c6e810b6489679c8e1d9fa1fb67e
RoboTrade Solutions <138616027+roboTradeSolutions@users.noreply.github.com>
2023-12-29 17:03:10 +0000

    dark default

--- ui/snippets/topBar/ColorModeSwitch.tsx
+++ ui/snippets/topBar/ColorModeSwitch.tsx
@@ -1,26 +1,16 @@
-import {
-  IconButton,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverBody,
-  useColorMode,
-  useDisclosure,
-  Skeleton,
-} from '@chakra-ui/react';
 import React from 'react';
 
 import * as cookies from 'lib/cookies';
-import IconSvg from 'ui/shared/IconSvg';
+// import IconSvg from 'ui/shared/IconSvg';
 
-import ColorModeSwitchTheme from './ColorModeSwitchTheme';
+// import ColorModeSwitchTheme from './ColorModeSwitchTheme';
 import { COLOR_THEMES } from './utils';
 
 const ColorModeSwitch = () => {
-  const { isOpen, onToggle, onClose } = useDisclosure();
-  const { setColorMode, colorMode } = useColorMode();
+  // const { isOpen, onToggle, onClose } = useDisclosure();
+  // const { setColorMode, colorMode } = useColorMode();
 
-  const [ activeHex, setActiveHex ] = React.useState<string>();
+  // const [ activeHex, setActiveHex ] = React.useState<string>();
 
   const setTheme = React.useCallback((hex: string) => {
     const nextTheme = COLOR_THEMES.find((theme) => theme.colors.some((color) => color.hex === hex));
@@ -29,68 +19,51 @@ const ColorModeSwitch = () => {
       return;
     }
 
-    setColorMode(nextTheme.colorMode);
+    // setColorMode(nextTheme.colorMode);
 
     const varName = nextTheme.colorMode === 'light' ? '--chakra-colors-white' : '--chakra-colors-black';
     window.document.documentElement.style.setProperty(varName, hex);
 
     cookies.set(cookies.NAMES.COLOR_MODE_HEX, hex);
-  }, [ setColorMode ]);
+  }, [ ]);
 
   React.useEffect(() => {
-    const cookieColorMode = cookies.get(cookies.NAMES.COLOR_MODE);
+    // const cookieColorMode = cookies.get(cookies.NAMES.COLOR_MODE);
 
-    const nextColorMode = (() => {
-      if (!cookieColorMode) {
-        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-      }
+    // const nextColorMode = (() => {
+    //   if (!cookieColorMode) {
+    //     return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    //   }
 
-      return colorMode;
-    })();
+    //   return colorMode;
+    // })();
 
-    const fallbackHex = (COLOR_THEMES.find(theme => theme.colorMode === nextColorMode && theme.colors.length === 1) ?? COLOR_THEMES[0]).colors[0].hex;
-    const cookieHex = cookies.get(cookies.NAMES.COLOR_MODE_HEX) ?? fallbackHex;
-    setTheme(cookieHex);
-    setActiveHex(cookieHex);
+    // const fallbackHex = (COLOR_THEMES.find(theme => theme.colorMode === nextColorMode && theme.colors.length === 1) ?? COLOR_THEMES[0]).colors[0].hex;
+    // const cookieHex = cookies.get(cookies.NAMES.COLOR_MODE_HEX) ?? fallbackHex;
+    setTheme('#101112');
+    // setActiveHex('#101112');
   // should run only on mount
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ ]);
 
-  const handleSelect = React.useCallback((event: React.MouseEvent<HTMLDivElement>) => {
-    event.stopPropagation();
+  // const handleSelect = React.useCallback((event: React.MouseEvent<HTMLDivElement>) => {
+  //   event.stopPropagation();
 
-    const hex = event.currentTarget.getAttribute('data-hex');
+  //   const hex = event.currentTarget.getAttribute('data-hex');
 
-    if (!hex) {
-      return;
-    }
+  //   if (!hex) {
+  //     return;
+  //   }
 
-    setTheme(hex);
-    setActiveHex(hex);
-  }, [ setTheme ]);
+  //   setTheme(hex);
+  //   setActiveHex(hex);
+  // }, [ setTheme ]);
 
-  const activeTheme = COLOR_THEMES.find((theme) => theme.colors.some((color) => color.hex === activeHex));
+  // const activeTheme = COLOR_THEMES.find((theme) => theme.colors.some((color) => color.hex === activeHex));
 
   return (
-    <Popover placement="bottom-start" isLazy trigger="click" isOpen={ isOpen } onClose={ onClose }>
-      <PopoverTrigger>
-        { activeTheme ? (
-          <IconButton
-            variant="simple"
-            colorScheme="blue"
-            aria-label="color mode switch"
-            icon={ <IconSvg name={ activeTheme.icon } boxSize={ 5 }/> }
-            boxSize={ 5 }
-            onClick={ onToggle }
-          />
-        ) : <Skeleton boxSize={ 5 } borderRadius="sm"/> }
-      </PopoverTrigger>
-      <PopoverContent overflowY="hidden" w="164px" fontSize="sm">
-        <PopoverBody boxShadow="2xl" p={ 3 }>
-          { COLOR_THEMES.map((theme) => <ColorModeSwitchTheme key={ theme.name } { ...theme } onClick={ handleSelect } activeHex={ activeHex }/>) }
-        </PopoverBody>
-      </PopoverContent>
-    </Popover>
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <></>
   );
 };
 
